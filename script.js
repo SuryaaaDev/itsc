7d0d2a237beb7e42b6915082efdae65c813ad310
Surya Saputra <157967533+SuryaaaDev@users.noreply.github.com>
2025-07-17 00:41:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menu');
+    var hamburger = document.querySelector('.hamburger');
+
+    menuToggle.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
